acd21e7ffc20ce497c9fcaeab7af1c6875b31822
Arlindo Da Silva Kohls <62305577+Kohls20@users.noreply.github.com>
2022-03-07 09:44:52 -0300

    Commit inicial, adicionando todos os arquivos

---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'Brasil'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {     //condição composta
+    console.log('Você é Brasileiro')
+}
+else{
+    console.log('Você é Estrangeiro')
+}
