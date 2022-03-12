a9d19986e8a8b09519677381dcc4c396ecc6a554
Arlindo Da Silva Kohls <62305577+Kohls20@users.noreply.github.com>
2022-03-12 17:51:55 -0300

    Adicionando aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+
+/*
+console.log(`C = ${c}`)
+console.log(`C = ${c}`)
+console.log(`C = ${c}`)
+console.log(`C = ${c}`)
+console.log(`C = ${c}`)
+console.log(`C = ${c}`)
+*/
+
+/*
+while (c <= 6){
+    console.log(`C = ${c}`)
+    c++
+} 
+*/
+
+
+do {
+    console.log(`C = ${c}`)
+    c++
+} while (c <= 6);
