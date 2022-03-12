0da17fa0d5c156cd094dfa9451ea85e23bfe708d
Arlindo Da Silva Kohls <62305577+Kohls20@users.noreply.github.com>
2022-03-12 18:46:49 -0300

    Adicionando aula 14

---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+
+while (c<=5) {
+    console.log(`C -> ${c}`)
+    c++
+}
+*/
+console.log("Vai começar...")
+for (var e = 1; e <= 4; e++) {
+    console.log(e) 
+}
+console.log("FIM!")
